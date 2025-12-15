bfcce89b5eb5437777c8fd53089ba8dfa9adef41
Pukar Subedi <subedipukar@gmail.com>
2025-12-15 18:54:14 +0000

    use getter fucntion for gemini_llm

--- backend/app/agents/rag/nodes.py
+++ backend/app/agents/rag/nodes.py
@@ -1,24 +1,21 @@
 """
 app/agents/rag/nodes.py
 
-Nodes for the RAG agent.
+Node fucntions for the RAG agent.
 """
 
 from langchain_core.messages import SystemMessage
 from langchain_core.runnables import RunnableConfig
 
-from app.config import SYSTEM_PROMPT, gemini_llm
+from app.config import SYSTEM_PROMPT, get_gemini_llm
 from app.agents.rag.tools import get_tools
 from app.agents.rag.state import RAGState
 
-model_with_tools = gemini_llm.bind_tools(get_tools())
+model_with_tools = get_gemini_llm().bind_tools(get_tools())
 
 def rag_node(state: RAGState, config: RunnableConfig):
     """
-    Generates AI reply and returns an update for state.
-`
-    Returns:
-        dict: Updated state with AI reply.
+    Returns an update for RAGState
     """
     history = state["messages"]
 
