bf6a040445d315924838ae27d203092be3c7a3f9
psubedi <subedipukar@gmail.com>
2026-06-08 13:33:34 -0400

    chore: format

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata = {
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: ReactNode;
+    children: ReactNode; 
 }) {
   return (
     <html lang="en">
